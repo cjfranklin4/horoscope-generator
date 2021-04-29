d7ef9f9781d81667a985b4ac331d8f61ebb7dc5f
Cayla Franklin <cjfranklin4@gmail.com>
2021-04-28 20:55:32 -0500

    Updates index.html and main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 document.querySelector(#submit).addEventListener('click', genHoro);
 
-let genHoro = () => {
-    
+const genHoro = () => {
+    const birthDay = document.querySelector(#day).value;
+    const birthMonth = document.querySelector(#month).value;
+    /*const birthYear = document.querySelector(#year).value;*/
+
+    switch(birthDate){
+        case birthDay:
+            //Capricon
+            document.querySelector(#horo_here).innerText = `You will eat food for dinner`
+            break;
+        case:
+            //Aquarius
+            document.querySelector(#horo_here).innerText = `Green is your best color`
+            break;
+        case:
+            //Leo??
+            document.querySelector(#horo_here).innerText = `Lions are cool`
+            break;
+        default:
+            document.querySelector(#horo_here).innerText = `Please put in your birthdate in the afformentied format.`
+
+    }
+
 }
