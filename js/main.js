a1ce441573696f2d9f472db589878497ed478a3b
Cayla Franklin <cjfranklin4@gmail.com>
2021-05-10 23:48:15 -0500

    Updated styling and main.js

--- js/main.js
+++ js/main.js
@@ -49,79 +49,67 @@ function horoscope() {
     //Posting Your Horoscope to the DOM
     if (horoDate > aquaStart && horoDate < aquaEnd){
         console.log('Aqua')
-        document.querySelector("#horo_here").innerText = `You are a Aquarius!
-
-        Be patient with yourself today you as may end up having a tough time managing relationships with those close to you.`
+        document.querySelector("#horo_here").innerText = `You are a Aquarius!`
+        document.querySelector("#horo_p").innerText=` Be patient with yourself today you as may end up having a tough time managing relationships with those close to you.`
         
     } else if (horoDate > piStart && horoDate < piEnd) {
         console.log('Pi')
-        document.querySelector("#horo_here").innerText = `You are a Pisces!
-
-        Small things will bring you the most happiness today. Take your time and appreciate them!`
+        document.querySelector("#horo_here").innerText = `You are a Pisces!`
+        document.querySelector("#horo_p").innerText=`Small things will bring you the most happiness today. Take your time and appreciate them!`
 
     } else if (horoDate > arStart && horoDate < arEnd) {
         console.log('Aries')
-        document.querySelector("#horo_here").innerText = `You are a Aries!
-
-        Keep yourself filled with hope and optimism. A positive mindset will help you handle your worries and bring about better outcomes in life.`
-
+        document.querySelector("#horo_here").innerText = `You are a Aries!`
+        document.querySelector("#horo_p").innerText=`Keep yourself filled with hope and optimism. A positive mindset will help you handle your worries and bring about better outcomes in life.`
 
     } else if (horoDate > taStart && horoDate < taEnd){
         console.log('Taurus')
-        document.querySelector("#horo_here").innerText = `You are a Taurus!
-
-        There is an exepected occurance waiting to throw you off your game today. But your calmness and maturity will keep you balanced.`
-
+        document.querySelector("#horo_here").innerText = `You are a Taurus!`
+        document.querySelector("#horo_p").innerText=`There is an exepected occurance waiting to throw you off your game today. But your calmness and maturity will keep you balanced.`
+    
     } else if (horoDate > gemStart && horoDate < gemEnd){
         console.log('Gemini')
-        document.querySelector("#horo_here").innerText = `You are a Gemini!
-
-        Feelings of insecurity may surface today, making you nervous. Try doing something that makes you happy to boost your confidence.`
-
+        document.querySelector("#horo_here").innerText = `You are a Gemini!`
+        document.querySelector("#horo_p").innerText=` Feelings of insecurity may surface today, making you nervous. Try doing something that makes you happy to boost your confidence.`
+    
     } else if (horoDate > canStart && horoDate < canEnd){
         console.log('Cancer')
-        document.querySelector("#horo_here").innerText = `You are a Cancer!
-
-
-        You will be showered with appreciation and praise for your efforts at work. You might even be able to get a promotion or a pay rise today.`
-
+        document.querySelector("#horo_here").innerText = `You are a Cancer!`
+        document.querySelector("#horo_p").innerText=`You will be showered with appreciation and praise for your efforts at work. You might even be able to get a promotion or a pay rise today.`
+   
     } else if (horoDate > leoStart && horoDate < leoEnd){
         console.log('Leo')
-        document.querySelector("#horo_here").innerText = `You are a Leo!
-
-        New relationships will get a chance to blossom today. Be open to talking with someone you may not know.`
-
+        document.querySelector("#horo_here").innerText = `You are a Leo!`
+        document.querySelector("#horo_p").innerText=`New relationships will get a chance to blossom today. Be open to talking with someone you may not know.`
+  
     } else if (horoDate > virStart && horoDate < virEnd){
         console.log('Virgo')
-        document.querySelector("#horo_here").innerText = `You are a Virgo!
-
-        You may hit a roadblock today and become stressed. Don't worry, though, as the stress will melt away as you spend time with your loved ones.`
-
+        document.querySelector("#horo_here").innerText = `You are a Virgo!`
+        document.querySelector("#horo_p").innerText=`You may hit a roadblock today and become stressed. Don't worry, though, as the stress will melt away as you spend time with your loved ones.`
+   
     } else if (horoDate > libStart && horoDate < libEnd){
         console.log('Libra')
-        document.querySelector("#horo_here").innerText = `You are a Libra!
-
-        The day may start off on a tense note but by evening, things will be peaceful and calm`
-
+        document.querySelector("#horo_here").innerText = `You are a Libra!`
+        document.querySelector("#horo_p").innerText=`The day may start off on a tense note but by evening, things will be peaceful and calm.`
+    
     } else if (horoDate > scoStart && horoDate < scoEnd){
         console.log('Scorpio')
-        document.querySelector("#horo_here").innerText = `You are a Scorpio!
-
-        Your plans for today will see major changes due to an unexpected occurance, but you make progress in everything you had set out to do.`
-
+        document.querySelector("#horo_here").innerText = `You are a Scorpio!`
+        document.querySelector("#horo_p").innerText=`Your plans for today will see major changes due to an unexpected occurance, but you make progress in everything you had set out to do.`
+    
     } else if (horoDate > sagStart && horoDate < sagEnd){
         console.log('Sagittarius')
-        document.querySelector("#horo_here").innerText = `You are a Sagittarius!
-
-        You will look past the criticism coming your way and make progress on your goals and dreams for the future.`
-
-    } else if (horoDate > capStart && horoDate < capEnd) {
-        document.querySelector("#horo_here").innerText = `You are a Capricorn!
-
-
-        You will understand the importance of constructive criticism and learn to use it to better yourself.`
-
+        document.querySelector("#horo_here").innerText = `You are a Sagittarius!`
+        document.querySelector("#horo_p").innerText=`You will look past the criticism coming your way and make progress on your goals and dreams for the future.`
+   
+    } else if (horoDate > capStart || horoDate < capEnd) {
+        document.querySelector("#horo_here").innerText = `You are a Capricorn!`
+        document.querySelector("#horo_p").innerText=`You will understand the importance of constructive criticism and learn to use it to better yourself.`
+   
     } else {
+        console.log(capStart)
+        console.log(capEnd)
+        console.log(horoDate)
         alert('Please enter a date.')
     }
     
